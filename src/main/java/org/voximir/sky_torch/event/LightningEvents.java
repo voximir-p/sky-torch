a11c4f56b965e3e8a0f7aee1c9903c0f3cdbd75a
voximir-p <pek795b@gmail.com>
2026-03-26 16:53:57 +0700

    feat: add burnt shard to supercharged shard transformation sound

--- src/main/java/org/voximir/sky_torch/event/LightningEvents.java
+++ src/main/java/org/voximir/sky_torch/event/LightningEvents.java
@@ -1,7 +1,10 @@
 package org.voximir.sky_torch.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.item.ItemStack;
 import org.voximir.sky_torch.item.ModItems;
@@ -19,6 +22,12 @@ public class LightningEvents {
 
             player.setItemInHand(player.getUsedItemHand(), new ItemStack(ModItems.SUPERCHARGED_SHARD));
             player.displayClientMessage(Translatable.component("overlay", "shard_supercharge"), true);
+
+            ServerLevel serverLevel = player.level();
+
+            serverLevel.playSound(null, player, SoundEvents.WARDEN_SONIC_BOOM, SoundSource.PLAYERS, 1.0f, 0.0f);
+            serverLevel.playSound(null, player, SoundEvents.WARDEN_SONIC_BOOM, SoundSource.PLAYERS, 1.0f, 1.0f);
+            serverLevel.playSound(null, player, SoundEvents.TOTEM_USE, SoundSource.PLAYERS, 1.0f, 0.5f);
         });
     }
 }
